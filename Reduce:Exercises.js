ca0e2961d1a8a91b5bf4541ed95f8c7fed69c55b
Schartelb <106553010+Schartelb@users.noreply.github.com>
2022-07-20 09:48:00 -0400

    Create Reduce:Exercises.js

---
+++ Reduce:Exercises.js
@@ -0,0 +1,10 @@
+//extractValue
+const arr = [{name: 'Elie'}, {name: 'Tim'}, {name: 'Matt'}, {name: 'Colt'}]
+const names=[]
+function extractValue(arr,name){
+	names=arr.reduce(function(value){
+		return arr.name
+	})
+}
+
+//vowelCount
