904e2d882437b3513ba32b1fa3cb838f99886b1a
Schartelb <106553010+Schartelb@users.noreply.github.com>
2022-07-20 14:27:03 -0400

    Update Reduce:Exercises.js
    practice problems did not go well, haha.
    
    Reviewing the overall functions for understanding

--- Reduce:Exercises.js
+++ Reduce:Exercises.js
@@ -8,3 +8,58 @@ function extractValue(arr,name){
 }
 
 //vowelCount
+
+//addKeyAndValue
+
+//partitionIsEven
+function partition(arr,call)
+
+
+//**ANSWERS**
+
+//function extractValue(arr, key){
+//    return arr.reduce(function(acc,next){
+//        acc.push(next[key]);
+//        return acc;
+//    },[]);
+//}
+
+//function vowelCount(str){
+//    const vowels = "aeiou";
+//    return str.split('').reduce(function(acc,next){
+//        let lowerCased = next.toLowerCase()
+//        if(vowels.indexOf(lowerCased) !== -1){
+//            if(acc[lowerCased]){
+//                acc[lowerCased]++;
+//            } else {
+//                acc[lowerCased] = 1;
+//            }
+//        }
+//        return acc;
+//    }, {});
+//}
+
+//function addKeyAndValue(arr, key, value){
+//    return arr.reduce(function(acc,next,idx){
+//        acc[idx][key] = value;
+//        return acc;
+//    },arr);
+//}
+
+//function addKeyAndValue(arr, key, value){
+//    return arr.reduce(function(acc,next,idx){
+//        acc[idx][key] = value;
+//        return acc;
+//    },arr);
+//}
+
+//function partition(arr, cb){
+//    return arr.reduce(function(acc,next){	
+//        if(cb(next)){      				if (callback(value)) is true
+//            acc[0].push(next);				push into first accumulator array
+//        } else {					otherwise
+//            acc[1].push(next);				push into second accumulator array
+//        }
+//        return acc;					return accumulator array
+//    }, [[],[]]);				reduce accumulator array starting with 2 empty arrays 
+//}
