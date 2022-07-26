cea4c7da8f7e08bf44ce787177188ec099bf8772
Schartelb <106553010+Schartelb@users.noreply.github.com>
2022-07-26 10:37:33 -0400

    attempt to answer

--- Arrow-Functions-Exercise.js
+++ Arrow-Functions-Exercise.js
@@ -1,1 +1,6 @@
-const double = (arr) => {arr.map((val)=>val*2)}
+const double = (arr) => {arr.map((val)=>{val*2})}
+
+const squareAndFindEvens = (numbers) => {
+    const squares = ({numbers.map((num)=>{num**2})})
+    const evens = squares.filter((square) => {square %2===0})
+    return evens}
