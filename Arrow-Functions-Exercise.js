eadf99f222453148594528cd89988fe31942986d
Schartelb <106553010+Schartelb@users.noreply.github.com>
2022-07-26 10:48:40 -0400

    Added corrected answers

--- Arrow-Functions-Exercise.js
+++ Arrow-Functions-Exercise.js
@@ -1,6 +1,10 @@
 const double = (arr) => {arr.map((val)=>{val*2})}
-
+//answer 
+//counst double = arr => arr.map(val => val*2)
 const squareAndFindEvens = (numbers) => {
     const squares = ({numbers.map((num)=>{num**2})})
     const evens = squares.filter((square) => {square %2===0})
     return evens}
+
+//answer
+//const squareAndFindEvens = numbers => numbers.map(val => val **2).filter(square => square %2===0)
