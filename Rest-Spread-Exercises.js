0a30a04c1267383fed41ce690430a3aa7ce25fb1
Schartelb <106553010+Schartelb@users.noreply.github.com>
2022-07-27 08:41:42 -0400

    Attempts up to Slice and Dice

--- Rest-Spread-Exercises.js
+++ Rest-Spread-Exercises.js
@@ -1,1 +1,16 @@
+function filterOutOdds(...nums){
+    nums.filter((num)=>{num%2===0})
+}
+
+const findMin=(...nums)=>{ Math.min(...nums)}
+
+function mergeObjects(obj1,obj2){
+    const newobj= [...obj1,...obj2]
+    return newobj
+}
+
+function doubleAndReturnArgs(arr,...rest){
+    const double = (...rest) => {rest*2}
+    return [...arr,...double]
+}
 
